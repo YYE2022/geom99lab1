b84784a8b9bb78115d644e14aef1b4cefaf6979a
YYE2022 <114246591+YYE2022@users.noreply.github.com>
2023-01-25 22:22:17 -0500

    Update maps.js

--- story/maps.js
+++ story/maps.js
@@ -8,6 +8,42 @@ const neighborhoods = [
   { lat: 64.49, lng: -18.48 },
 
 ];  
+
+let markers = [];
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 64.9631, lng: -19.0208 },
+  });
+  document.getElementById("drop").addEventListener("click", drop);
+}
+
+function drop() {
+  clearMarkers();
+
+  for (let i = 0; i < neighborhoods.length; i++) {
+    addMarkerWithTimeout(neighborhoods[i], i * 200);
+  }
+}
+
+function addMarkerWithTimeout(position, timeout) {
+  window.setTimeout(() => {
+    markers.push(
+      new google.maps.Marker({
+        position: position,
+        map,
+        animation: google.maps.Animation.DROP,
+      })
+    );
+  }, timeout);
+}
+
+function clearMarkers() {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
 const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
@@ -46,42 +82,6 @@ const marker = new google.maps.Marker({
       map,
     });
   });
-let markers = [];
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 64.9631, lng: -19.0208 },
-  });
-  document.getElementById("drop").addEventListener("click", drop);
-}
-
-function drop() {
-  clearMarkers();
-
-  for (let i = 0; i < neighborhoods.length; i++) {
-    addMarkerWithTimeout(neighborhoods[i], i * 200);
-  }
-}
-
-function addMarkerWithTimeout(position, timeout) {
-  window.setTimeout(() => {
-    markers.push(
-      new google.maps.Marker({
-        position: position,
-        map,
-        animation: google.maps.Animation.DROP,
-      })
-    );
-  }, timeout);
-}
-
-function clearMarkers() {
-  for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
-
   markers = [];
 }
 
