b0278283a24dd561de3cab19cbd94993fd8682af
YYE2022 <114246591+YYE2022@users.noreply.github.com>
2023-01-25 22:49:33 -0500

    Update maps.js

--- story/maps.js
+++ story/maps.js
@@ -1,51 +1,89 @@
-
-
-// If you're adding a number of markers, you may want to drop them on the map
-// consecutively rather than all at once. This example shows how to use
-// window.setTimeout() to space your markers' animation.
-const neighborhoods = [
-  { lat: 64.1466, lng: -21.9426 },
-  { lat: 64.49, lng: -18.48 },
-
-];  
-
-let markers = [];
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 64.9631, lng: -19.0208 },
+  const origin = { lat: 43.7754, lng:-79.2578 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: origin,
   });
-  document.getElementById("drop").addEventListener("click", drop);
+
+  new ClickEventHandler(map, origin);
 }
 
-function drop() {
-  clearMarkers();
+function isIconMouseEvent(e) {
+  return "placeId" in e;
+}
 
-  for (let i = 0; i < neighborhoods.length; i++) {
-    addMarkerWithTimeout(neighborhoods[i], i * 200);
+class ClickEventHandler {
+  origin;
+  map;
+  directionsService;
+  directionsRenderer;
+  placesService;
+  infowindow;
+  infowindowContent;
+  constructor(map, origin) {
+    this.origin = origin;
+    this.map = map;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+    this.placesService = new google.maps.places.PlacesService(map);
+    this.infowindow = new google.maps.InfoWindow();
+    this.infowindowContent = document.getElementById("infowindow-content");
+    this.infowindow.setContent(this.infowindowContent);
+    // Listen for clicks on the map.
+    this.map.addListener("click", this.handleClick.bind(this));
   }
-}
+  handleClick(event) {
+    console.log("You clicked on: " + event.latLng);
+    // If the event has a placeId, use it.
+    if (isIconMouseEvent(event)) {
+      console.log("You clicked on place:" + event.placeId);
+      // Calling e.stop() on the event prevents the default info window from
+      // showing.
+      // If you call stop here when there is no placeId you will prevent some
+      // other map click event handlers from receiving the event.
+      event.stop();
+      if (event.placeId) {
+        this.calculateAndDisplayRoute(event.placeId);
+        this.getPlaceInformation(event.placeId);
+      }
+    }
+  }
+  calculateAndDisplayRoute(placeId) {
+    const me = this;
 
-function addMarkerWithTimeout(position, timeout) {
-  window.setTimeout(() => {
-    markers.push(
-      new google.maps.Marker({
-        position: position,
-        map,
-        animation: google.maps.Animation.DROP,
+    this.directionsService
+      .route({
+        origin: this.origin,
+        destination: { placeId: placeId },
+        travelMode: google.maps.TravelMode.WALKING,
       })
-    );
-  }, timeout);
-}
-
-function clearMarkers() {
-  for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
+      .then((response) => {
+        me.directionsRenderer.setDirections(response);
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
   }
+  getPlaceInformation(placeId) {
+    const me = this;
 
-  markers = [];
+    this.placesService.getDetails({ placeId: placeId }, (place, status) => {
+      if (
+        status === "OK" &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        me.infowindow.close();
+        me.infowindow.setPosition(place.geometry.location);
+        me.infowindowContent.children["place-icon"].src = place.icon;
+        me.infowindowContent.children["place-name"].textContent = place.name;
+        me.infowindowContent.children["place-id"].textContent = place.place_id;
+        me.infowindowContent.children["place-address"].textContent =
+          place.formatted_address;
+        me.infowindow.open(me.map);
+      }
+    });
+  }
 }
 
 window.initMap = initMap;
