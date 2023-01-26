e1100194fe094c03bff19d277c4174ac6fb12f28
YYE2022 <114246591+YYE2022@users.noreply.github.com>
2023-01-25 23:07:15 -0500

    Update maps.js

--- story/maps.js
+++ story/maps.js
@@ -1,89 +1,43 @@
 function initMap() {
-  const origin = { lat: 43.7754, lng:-79.2578 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 18,
-    center: origin,
+    zoom: 8,
+    center: { lat: 63.333, lng: -150.5 },
+    mapTypeId: "terrain",
   });
+  const elevator = new google.maps.ElevationService();
+  const infowindow = new google.maps.InfoWindow({});
 
-  new ClickEventHandler(map, origin);
+  infowindow.open(map);
+  // Add a listener for the click event. Display the elevation for the LatLng of
+  // the click inside the infowindow.
+  map.addListener("click", (event) => {
+    displayLocationElevation(event.latLng, elevator, infowindow);
+  });
 }
 
-function isIconMouseEvent(e) {
-  return "placeId" in e;
-}
-
-class ClickEventHandler {
-  origin;
-  map;
-  directionsService;
-  directionsRenderer;
-  placesService;
-  infowindow;
-  infowindowContent;
-  constructor(map, origin) {
-    this.origin = origin;
-    this.map = map;
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-    this.directionsRenderer.setMap(map);
-    this.placesService = new google.maps.places.PlacesService(map);
-    this.infowindow = new google.maps.InfoWindow();
-    this.infowindowContent = document.getElementById("infowindow-content");
-    this.infowindow.setContent(this.infowindowContent);
-    // Listen for clicks on the map.
-    this.map.addListener("click", this.handleClick.bind(this));
-  }
-  handleClick(event) {
-    console.log("You clicked on: " + event.latLng);
-    // If the event has a placeId, use it.
-    if (isIconMouseEvent(event)) {
-      console.log("You clicked on place:" + event.placeId);
-      // Calling e.stop() on the event prevents the default info window from
-      // showing.
-      // If you call stop here when there is no placeId you will prevent some
-      // other map click event handlers from receiving the event.
-      event.stop();
-      if (event.placeId) {
-        this.calculateAndDisplayRoute(event.placeId);
-        this.getPlaceInformation(event.placeId);
+function displayLocationElevation(location, elevator, infowindow) {
+  // Initiate the location request
+  elevator
+    .getElevationForLocations({
+      locations: [location],
+    })
+    .then(({ results }) => {
+      infowindow.setPosition(location);
+      // Retrieve the first result
+      if (results[0]) {
+        // Open the infowindow indicating the elevation at the clicked position.
+        infowindow.setContent(
+          "The elevation at this point <br>is " +
+            results[0].elevation +
+            " meters."
+        );
+      } else {
+        infowindow.setContent("No results found");
       }
-    }
-  }
-  calculateAndDisplayRoute(placeId) {
-    const me = this;
-
-    this.directionsService
-      .route({
-        origin: this.origin,
-        destination: { placeId: placeId },
-        travelMode: google.maps.TravelMode.WALKING,
-      })
-      .then((response) => {
-        me.directionsRenderer.setDirections(response);
-      })
-      .catch((e) => window.alert("Directions request failed due to " + status));
-  }
-  getPlaceInformation(placeId) {
-    const me = this;
-
-    this.placesService.getDetails({ placeId: placeId }, (place, status) => {
-      if (
-        status === "OK" &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-      ) {
-        me.infowindow.close();
-        me.infowindow.setPosition(place.geometry.location);
-        me.infowindowContent.children["place-icon"].src = place.icon;
-        me.infowindowContent.children["place-name"].textContent = place.name;
-        me.infowindowContent.children["place-id"].textContent = place.place_id;
-        me.infowindowContent.children["place-address"].textContent =
-          place.formatted_address;
-        me.infowindow.open(me.map);
-      }
-    });
-  }
+    })
+    .catch((e) =>
+      infowindow.setContent("Elevation service failed due to: " + e)
+    );
 }
 
 window.initMap = initMap;
